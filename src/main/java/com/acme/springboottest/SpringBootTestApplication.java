1af8902fa66aa135dd31853efd6a366d0187b52f
genebarringer <gene.barringer@gmail.com>
2020-09-02 15:36:58 -0500

    Initial commit of a test project

---
+++ src/main/java/com/acme/springboottest/SpringBootTestApplication.java
@@ -0,0 +1,13 @@
+package com.acme.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestApplication.class, args);
+    }
+
+}
